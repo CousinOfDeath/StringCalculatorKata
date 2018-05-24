febc408f18f56f4e90cb5d999245663c61144651
Unknown <makkonenmiska@gmail.com>
2018-05-24 21:14:29 +0300

    [WIP] Covered more cases

--- StringCalculatorKata.Tests/StringCalculatorKataTests.cs
+++ StringCalculatorKata.Tests/StringCalculatorKataTests.cs
@@ -4,12 +4,17 @@ using NUnit.Framework;
 namespace StringCalculatorKata.Tests
 {
     [TestFixture]
-    public class StringCalculatorKataTests
+    public sealed class StringCalculatorKataTests
     {
+        private StringCalculator CreateStringCalculator()
+        {
+            return new StringCalculator();
+        }
+        
         [Test]
         public void Add_WhenEmptyString_ReturnDefault()
         {
-            var sc = new StringCalculator();
+            var sc = CreateStringCalculator();
             
             var result = sc.Add("");
             
@@ -19,7 +24,7 @@ namespace StringCalculatorKata.Tests
         [Test]
         public void Add_WhenEmptyPositiveNumber_ReturnThatValue()
         {
-            var sc = new StringCalculator();
+            var sc = CreateStringCalculator();
             
             var result = sc.Add("1");
             
@@ -29,12 +34,44 @@ namespace StringCalculatorKata.Tests
         [Test]
         public void Add_WhenEmptyPositiveNumbers_ReturnSum()
         {
-            var sc = new StringCalculator();
+            var sc = CreateStringCalculator();
             
             var result = sc.Add("1,2");
             
             Assert.AreEqual(3, result);
         }
+
+
+        [Test]
+        public void Add_CustomDelimiter_ReturnsSum()
+        {
+            var sc = CreateStringCalculator();
+
+            var result = sc.Add("//;\n1;2");
+            
+            Assert.AreEqual(3, result);
+        }
+
+        [Test]
+        public void Add_WithNegativeNumber_ThrowsArgumentException()
+        {
+            var sc = CreateStringCalculator();
+
+            var e = Assert.Catch<ArgumentException>(() => { sc.Add("-1"); });
+            
+            StringAssert.Contains("negatives not allowed: -1", e.Message);
+        }
+
+        [Test]
+        public void Add_WithNegativeNumbers_ThrowsArgumentException()
+        {
+            var sc = CreateStringCalculator();
+
+            var e = Assert.Catch<ArgumentException>(() => { sc.Add("-1,-2"); });
+            
+            StringAssert.Contains("negatives not allowed: -1,-2", e.Message);
+        }
+
     }
     
 }
--- StringCalculatorKata/StringCalculator.cs
+++ StringCalculatorKata/StringCalculator.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace StringCalculatorKata
@@ -8,20 +9,83 @@ namespace StringCalculatorKata
 
         public int Add(string numbers)
         {
-            if (string.IsNullOrEmpty(numbers))
+            if (IsDefault(numbers))
             {
-                return 0;
+                return GetDefaultValue();
             }
 
-            if (numbers.Contains(","))
+            if (HasMultipleNumers(numbers))
             {
-                var numbersString = numbers.Split(',');
-
-                return numbersString.Select(int.Parse).Sum();
-
+                return GetSum(numbers);
             }
             
-            return int.Parse(numbers);
+            return ParseSingle(numbers);
+        }
+
+        protected virtual int ParseSingle(string numbers)
+        {
+            var num = int.Parse(numbers);
+
+            CheckThatNumberIsPositive(num);
+            
+            return num;
+        }
+
+        protected virtual void CheckThatNumberIsPositive(int num)
+        {
+            if (num < 0)
+            {
+                throw new ArgumentException($"negatives not allowed: {num}");
+            }
+        }
+
+        protected virtual int GetSum(string numbers)
+        {
+
+            var delimiters = new List<string>
+            {
+                "\n"
+            };
+            
+            if (numbers.StartsWith("//"))
+            {
+                var customDelimiter = numbers.Substring(2, 1);
+                delimiters.Add(customDelimiter);
+
+                numbers = numbers.Substring(4, numbers.Length - 4);
+            }
+
+            foreach (var delimiter in delimiters)
+            {
+                numbers = numbers.Replace(delimiter, ",");  
+            }
+
+            var numbersString = numbers.Split(',');
+            var numbersInt = numbersString.Select(int.Parse);
+            var negativeNumbers = numbersInt.Where(n => n < 0).ToList();
+
+            if (negativeNumbers.Any())
+            {
+                throw new ArgumentException($"negatives not allowed: {string.Join(",", negativeNumbers)}");
+            }
+            
+            return numbersString.Select(int.Parse).Sum();
+        }
+
+        protected virtual bool HasMultipleNumers(string numbers)
+        {
+            
+            return numbers.Contains(",") || numbers.StartsWith("//");
+        }
+
+        protected virtual int GetDefaultValue()
+        {
+            return 0;
+        }
+
+        protected virtual bool IsDefault(string numbers)
+        {
+            return string.IsNullOrEmpty(numbers);
         }
     }
 }
